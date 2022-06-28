e91ad74ec73ec67618ca121cfc4b30344a74beda
Yashwant <yashwant317201@gmail.com>
2022-06-28 15:51:36 +0530

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p >Made by <a href="https://yashwant-portfolio.netlify.app/">Yashwant</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
